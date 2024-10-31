ff13fd874a91f0b63ba17c9f31514d1e1553a8d8
Vlad <vasvladikus@gmail.com>
2024-10-31 16:15:40 +0100

    Product.cpp

--- Lesson9_3/Product.cpp
+++ Lesson9_3/Product.cpp
@@ -1,1 +1,61 @@
 #include "Product.h"
+
+Products::Product::Product()
+{
+    name = "undefined";
+    price = 0;
+    manufacturer = "undefined";
+    age = 0;
+}
+
+Products::Product::Product(std::string name, float price, std::string manufacturer, int age)
+{
+    this->name = name;
+    this->price = price;
+    this->manufacturer = manufacturer;
+    this->age = age;
+}
+
+Products::Product::~Product()
+{
+}
+
+void Products::Product::setName(string name)
+{
+    this->name = name;
+}
+
+void Products::Product::setPrice(float price)
+{
+    this->price - price;
+}
+
+void Products::Product::setManufacturer(string manufacturer)
+{
+    this->manufacturer = manufacturer;
+}
+
+void Products::Product::setAge(int age)
+{
+    this->age = age;
+}
+
+string Products::Product::getName() const
+{
+    return name;
+}
+
+float Products::Product::getPrice() const
+{
+    return price;
+}
+
+string Products::Product::getManufacturer() const
+{
+    return manufacturer;
+}
+
+int Products::Product::getAge() const
+{
+    return age;
+}
